78ee0c01c7f130108e2b448e49aa567568660046
Vagif_İsmayilov <134787499+VagifIsma@users.noreply.github.com>
2025-12-20 18:02:23 +0400

    sdfg

--- Новая папка (2)/config.py
+++ Новая папка (2)/config.py
@@ -4,7 +4,7 @@
 API_TOKEN = "02f42812-62c4-49d3-a1ff-19c6bdf1e683"
 
 # URL сервера (меняй на продакшн при необходимости)
-BASE_URL = "https://games-test.datsteam.dev"
+BASE_URL = "https://games.datsteam.dev"
 # BASE_URL = "https://games.datsteam.dev"
 
 # Настройки логирования
